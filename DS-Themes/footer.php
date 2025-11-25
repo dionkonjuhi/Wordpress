1b07ebe09cb725327181dccfcf25ae8af86fba58
dionkonjuhi <dionkonjuhiii70@gmail.com>
2025-11-25 17:34:29 +0100

    comit

---
+++ DS-Themes/footer.php
@@ -0,0 +1,8 @@
+ <footer>
+        <p>Digital school</p>
+        <nav class="footer-menu">
+        <?php  wp_nav_array( array('theme_location' => 'wp_devs_footer_menu', 'depth' => 1)); ?>
+        </nav>
+    </footer>
+</body>
+</html>
